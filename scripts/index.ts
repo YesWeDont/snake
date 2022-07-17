56ee6293ecc84160911c8991123ea01fbab727ab
YesWeDont <47885665+YesWeDont@users.noreply.github.com>
2022-07-17 14:45:49 +1000

    revert queue length fix

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,7 +4,7 @@ import { initiate } from "./prepare.js";
     let GAME = await initiate((direction)=>{
         //circumvent self-hitting + shorten queue
         let latestDirection = GAME.state.headingQueue[GAME.state.headingQueue.length - 1] || GAME.state.heading;
-        if(validDirectionChange(direction, latestDirection) && game.headingQueue.length < 3) GAME.state.headingQueue.push(direction)
+        if(validDirectionChange(direction, latestDirection)) GAME.state.headingQueue.push(direction)
     });
     //raf shoot my foot
     requestAnimationFrame(update)
